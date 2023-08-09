c3c9de255241fceb470e6db6262977dcfddb5ddb
Dimitri-byte <59877223+Dimitri-byte@users.noreply.github.com>
2023-08-09 19:33:53 +0200

    init

---
+++ IA_WebCreator/bin/IA.py
@@ -0,0 +1,28 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
+
+tokenizer = AutoTokenizer.from_pretrained("upstage/llama-30b-instruct")
+# model = AutoModelForCausalLM.from_pretrained(
+#     "upstage/llama-30b-instruct",
+#     offload_folder="/srv/IA_WebCreator/tmp",
+#     device_map="auto",
+#     torch_dtype=torch.float16,
+#     # load_in_8bit=True, # utilisation du gpu
+#     rope_scaling={"type": "dynamic", "factor": 2} # allows handling of longer inputs
+# )
+
+model = AutoModelForCausalLM.from_pretrained(
+    "/srv/IA_WebCreator/models/llama-30b-instruct-2048",
+    offload_folder="/srv/IA_WebCreator/tmp",
+    device_map="auto",
+    torch_dtype=torch.float16,
+    rope_scaling={"type": "dynamic", "factor": 2}
+)
+
+prompt = "### User:\nThomas is healthy, but he has to go to the hospital. What could be the reasons?\n\n### Assistant:\n"
+inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+del inputs["token_type_ids"]
+streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+output = model.generate(**inputs, streamer=streamer, use_cache=True, max_new_tokens=float('inf'))
+output_text = tokenizer.decode(output[0], skip_special_tokens=True)
---
+++ README.md
@@ -0,0 +1,39 @@
+# hackaton_2023
+## Prérequis
+- Docker
+- Docker-compose
+## Lancement du docker
+Se mettre dans le même répertoire que le fichier "docker-compose.yml".
+Lancer la commande suivante : 
+docker-compose up
+
+## Stoper le docker
+docker-compose stop
+
+## Lister les docker
+docker ps
+
+## Se connecter au docker
+### Apache
+docker exec -it hackaton_2023-apache-container-1 bash
+### Debian
+docker exec -it hackaton_2023-debian-container-1 bash
+
+## Supprimer le docker et vider le cache
+Se mettre dans le même répertoire que le fichier "docker-compose.yml".
+
+Lancer les commandes suivante dans l'ordre:
+docker-compose down
+docker rmi --force httpd:2.4
+docker rmi --force hackaton_2023-debian-container
+docker volume prune
+docker builder prune
+
+# Models
+## Télécharger les modèles de langages dans le répertoire 
+IA_WebCreator\models
+### Exemple
+IA_WebCreator\models\llama-30b-instruct-2048
+
+## Llama-2-70b-instruct-v2
+https://huggingface.co/upstage/Llama-2-70b-instruct-v2
