da39686553433f801db07092e17f0a328f51aed1
biard <biarddimitri@gmail.com>
2023-09-09 11:15:27 +0200

    Correction de l'erreur 415

--- angular/src/app/generate-text.service.ts
+++ angular/src/app/generate-text.service.ts
@@ -1,19 +1,20 @@
-import {Injectable} from "@angular/core";
-import {Observable} from "rxjs";
-import {HttpClient} from "@angular/common/http";
-import {ResponseText} from "./generate-text";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { ResponseText } from "./generate-text";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class GenerateTextService {
 
-    private url = 'http://localhost:5000/generate-text';
+  private url = 'http://localhost:5000/generate-text';
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-    public generate(message: string): Observable<ResponseText> {
-        return this.http.post<ResponseText>(this.url, null,{params: { prompt: message }});
-    }
-
+  public generate(message: string): Observable<ResponseText> {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const params = new HttpParams().set('prompt', message);
+    return this.http.post<ResponseText>(this.url, null, { headers, params });
+  }
 }
