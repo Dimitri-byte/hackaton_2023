825502cf0dc2e8d1e224ad79420a792f3cabd98b
CEGIDGROUP\drumond <rdrumond@cegid.com>
2023-09-12 11:14:44 +0200

    DEV : maj enlever le '\n' et '\' : test ok

--- IA_WebCreator/bin/IA.py
+++ IA_WebCreator/bin/IA.py
@@ -7,11 +7,11 @@ from flask_cors import CORS
 
 with open("/srv/IA_WebCreator/bin/api_key.txt", "r") as file:
     open_api_key = file.read().strip()
-contextPrompt = "As a professional front end developer, create an HTML and CSS skeleton with responsive design using Bootstrap for the following scenario."
-resumeTitlePrompt = "Also, please create a summary of the following scenario to be returned as the title"
-formattingPrompt = "Return the answer as a RFC8259 compliant JSON object with the following format."
-jsonFormatString = "[{\"htmlCode\": \"html code\", \"cssCode\": \"css code\", \"title\": \"summary of the scenario\"}]"
-cleaningUpJsonPrompt = "Remove all occurrences of \\n \\ and \\\ in your response."
+contextPrompt = 'As a professional front end developer, create an HTML and CSS skeleton with responsive design using Bootstrap for the following scenario.'
+resumeTitlePrompt = 'Also, please create a summary of the following scenario to be returned as the title'
+formattingPrompt = 'Return the answer as a JSON object with the following format: '
+jsonFormatString = '[{"htmlCode": "html code", "title": "summary of the scenario"}]'
+cleaningUpJsonPrompt = 'Remove all occurrences of \\n and \\ and \\\ in your response.'
 
 def generate_text(prompt):
     api_key = open_api_key
@@ -40,12 +40,11 @@ def generate_text(prompt):
     response = requests.post(endpoint, headers=headers, json=payload)
     if response.status_code == 200:
         completions = response.json()["choices"][0]["message"]["content"]
-        print("AI's response:", completions)
-        print("AI's response in JSON:", response.text)
+        #print(f"completions: {completions}")   
         return completions
     else:
-        print("Request failed with status code:", response.status_code)
-        print("Response:", response.text)
+        print(f"Request failed with status code: {response.status_code}")
+        print(f"response.text: {response.text}")
         return None
 
 app = Flask(__name__)
@@ -55,23 +54,16 @@ CORS(app)
 def generate_text_api():
     prompt = request.json.get('prompt')
     generated_text = generate_text(prompt)
-    # print(str(generated_text))
-    # #check data type with type() method
-    # print(type(str(generated_text)))
-    # generated_text_str = str(generated_text)
+    generated_text = generated_text.replace('\n', '')
+    generated_text = generated_text.replace('\\', '')
+    print(f"generated_text: {generated_text}")  
 
-    # data = {}
-    # data['key'] = 'value'
-    # json_data = json.dumps(generated_text_str)
-    #convert string to object
-    #TODO: corriger erreur convert type NONE to json (problème d'encodage?)
-    #generated_text_str.decode("utf-8")
-    # json_object = json.loads(generated_text_str.decode("utf-8"))
     response = {
         'generated_text': generated_text
     }
 
-    return jsonify(response)
+    #return jsonify(response)
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
